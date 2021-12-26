896899b14b7ab5202631a021f1cdf3981f780ed3
Atul Kumar <atulsaini649@gmail.com>
2021-12-26 10:02:19 +0530

    Design Changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import styled from 'styled-components';
-import React,{useState} from 'react';
+import styled from "styled-components";
+import React, { useState } from "react";
 import "./App.css";
-import axios from 'axios';
-import MovieComponent from './components/MovieComponent';
-import MovieInfoComponent from './components/MovieInfoComponent';
+import axios from "axios";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
 
-export const API_KEY = "eaa7d671"
+export const API_KEY = "eaa7d671";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Header = styled.div`
   display: flex;
   flex-direction: row;
-  background-color: black;
+  background-color: rgb(41, 50, 65);
   color: white;
   align-items: center;
   padding: 5px;
   box-shadow: 0 3px 6px 0 #555;
   justify-content: space-between;
   height: 70px;
-`
+`;
 
 const AppName = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   font-size: 23px;
-  color: rgb(0,175,178);
-`
+  color: rgb(0, 175, 178);
+  @media (max-width: 430px) {
+    font-size: 18px;
+  }
+`;
 
 const Logo = styled.img`
   width: 45px;
   height: 45px;
   margin: 15px;
   margin-left: 20px;
-`
+  @media (max-width: 430px) {
+    width: 34px;
+    height: 34px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+`;
 
 const SearchBox = styled.div`
   display: flex;
@@ -50,13 +59,12 @@ const SearchBox = styled.div`
   align-items: center;
   margin-right: 20px;
   height: 40px;
-  
-`
+`;
 
 const SearchIcon = styled.img`
   width: 18px;
   height: 18px;
-`
+`;
 
 const SearchInput = styled.input`
   width: 89%;
@@ -66,60 +74,114 @@ const SearchInput = styled.input`
   margin-left: 9px;
   outline: none;
   border: none;
-`
+  @media (max-width: 574px) {
+    font-size: 15px;
+  }
+  @media (max-width: 504px) {
+    font-size: 14px;
+  }
+`;
 
 const MovieListContainer = styled.div`
- display: flex;
- flex-direction: row;
- flex-wrap: wrap;
- justify-content: space-evenly;
- padding: 30px;
-`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 30px;
+`;
+
+const MainCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  vertical-align: middle;
+  justify-content: center;
+  margin-top: 130px;
+`;
 
 const Placeholder = styled.img`
   width: 60px;
   height: 60px;
-  margin: 150px;
-  opacity: 60%;
-`
+  opacity: 80%;
+  margin-bottom: 10px;
+  @media (max-width: 641px) {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+const Message = styled.div`
+  font-size: 40px;
+  font-weight: 500;
+  color: grey;
+  word-spacing: 4px;
+  @media (max-width: 641px) {
+    font-size: 27px;
+  }
+`;
 
 function App() {
-  const[searchQuery, updateSearchQuery] = useState();
-  const[timeoutId, updateTimeoutId] = useState();
-  const[movieList, updateMovieList] = useState();
-  const[selectedMovie, onMovieSelect] = useState();
+  const [searchQuery, updateSearchQuery] = useState();
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updateMovieList] = useState();
+  const [selectedMovie, onMovieSelect] = useState();
 
-  const fetchData = async(searchString) => {
-    const response = await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`)
+  const fetchData = async (searchString) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+    );
     console.log(response);
-    updateMovieList(response.data.Search)
-  }
+    updateMovieList(response.data.Search);
+  };
 
   const onTextChange = (event) => {
     clearTimeout(timeoutId);
     updateSearchQuery(event.target.value);
-    const timeout = setTimeout(() => fetchData(event.target.value),1000);
+    const timeout = setTimeout(() => fetchData(event.target.value), 1000);
     updateTimeoutId(timeout);
-  }
-  
+  };
+
   return (
-  <Container>
-    <Header>
-      <AppName>
-        <Logo src='/logo.png'/>
-        Moviemania
+    <Container>
+      <Header>
+        <AppName>
+          <Logo src="/logo.png" />
+          Moviemania
         </AppName>
-      <SearchBox>
-        <SearchIcon src='/search.png'/>
-        <SearchInput placeholder = 'Search Moviemania' value={searchQuery} onChange={onTextChange}/>
-      </SearchBox>
-    </Header>
-    {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
-    <MovieListContainer>
-      {movieList?.length ? movieList.map((movie, index)=><MovieComponent key={index} movie={movie} onMovieSelect={onMovieSelect}/>):(<Placeholder src="/logo.png" />)
-      }
-    </MovieListContainer>
-  </Container>
+        <SearchBox>
+          <SearchIcon src="/search.png" />
+          <SearchInput
+            placeholder="Search Moviemania"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && (
+        <MovieInfoComponent
+          selectedMovie={selectedMovie}
+          onMovieSelect={onMovieSelect}
+        />
+      )}
+      <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <>
+            <MainCenter>
+              <Placeholder src="/logo.png" />
+              <Message>Search something</Message>
+            </MainCenter>
+          </>
+        )}
+      </MovieListContainer>
+    </Container>
   );
 }
 
--- src/components/MovieInfoComponent.js
+++ src/components/MovieInfoComponent.js
@@ -1,30 +1,50 @@
-import axios from 'axios';
-import React,{useEffect, useState} from 'react';
-import styled from 'styled-components';
-import { API_KEY } from '../App';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { API_KEY } from "../App";
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
-  padding: 20px 30px;  
+  padding: 20px 30px;
+  align-items: center;
   border-bottom: 1px solid lightgray;
-`
+  @media (max-width: 602px) {
+    flex-direction: column;
+  }
+`;
 
 const CoverImage = styled.img`
-  height: 270px;
-  /* width: 270px; */
+  height: 300px;
+  width: 270px;
   object-fit: cover;
   margin-left: 40px;
   margin-right: 30px;
-`
+  @media (max-width: 602px) {
+    width: 230px;
+    margin: auto;
+    height: 300px;
+  }
+  @media (max-width: 330px) {
+    width: 200px;
+    height: 290px;
+  }
+`;
 const InfoColumn = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 10px;
-`
+  @media (max-width: 602px) {
+    margin-left: 20px;
+    margin-top: 20px;
+  }
+  @media (max-width: 330px) {
+    margin-left: 10px;
+  }
+`;
 
 const MovieName = styled.span`
-  font-size: 16px;
+  font-size: 18px;
   font-weight: bold;
   color: black;
   overflow: hidden;
@@ -34,7 +54,14 @@ const MovieName = styled.span`
   & span {
     opacity: 0.8;
   }
-`
+  @media (max-width: 602px) {
+    text-align: center;
+    font-size: 21px;
+  }
+  @media (max-width: 330px) {
+    font-size: 18px;
+  }
+`;
 
 const MovieInfo = styled.span`
   font-size: 14px;
@@ -47,49 +74,65 @@ const MovieInfo = styled.span`
   & span {
     opacity: 0.5;
   }
-`
+`;
 
 const Close = styled.span`
-  font-size: 25px;
-  font-weight: 600;
-  color: black;
-  /* background: lightgray;  */
-  height: fit-content;
-  padding: 8px;
-  cursor: pointer;
-  opacity: 0.8;
+  display: none;
 `;
 
 function MovieInfoComponent(props) {
-  const[movieInfo,setMovieInfo] = useState();
-  const {selectedMovie} = props;
-  useEffect(()=>{
+  const [movieInfo, setMovieInfo] = useState();
+  const { selectedMovie } = props;
+  useEffect(() => {
     axios
-    .get(`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`)
-    .then((response)=> setMovieInfo(response.data));
-  },[selectedMovie]);
-    
+      .get(`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`)
+      .then((response) => setMovieInfo(response.data));
+  }, [selectedMovie]);
+
   return (
     <Container>
-      {movieInfo?<>
-        <CoverImage src={movieInfo?.Poster}/>
-        <InfoColumn>
-        <MovieName>{movieInfo?.Type}: {movieInfo?.Title}</MovieName>
-        <MovieInfo>IMDB Rating: <span>{movieInfo?.imdbRating}</span></MovieInfo>
-        <MovieInfo>Language: <span>{movieInfo?.Language}</span></MovieInfo>
-        <MovieInfo>Rated: <span>{movieInfo?.Rated}</span></MovieInfo>
-        <MovieInfo>Release: <span>{movieInfo?.Release}</span></MovieInfo>
-        <MovieInfo>Runtime: <span>{movieInfo?.Runtime}</span></MovieInfo>
-        <MovieInfo>Genre: <span>{movieInfo?.Genre}</span></MovieInfo>
-        <MovieInfo>Director: <span>{movieInfo?.Director}</span></MovieInfo>
-        <MovieInfo>Actors: <span>{movieInfo?.Actors}</span></MovieInfo>
-        <MovieInfo>Plot: <span>{movieInfo?.Plot}</span></MovieInfo>
-        </InfoColumn>
-        <Close onClick={() => props.onMovieSelect()}>X</Close>
-      </>:"Loading..."}
-      
+      {movieInfo ? (
+        <>
+          <CoverImage src={movieInfo?.Poster} />
+          <InfoColumn>
+            <MovieName>
+              {movieInfo?.Type}: {movieInfo?.Title}
+            </MovieName>
+            <MovieInfo>
+              IMDB Rating: <span>{movieInfo?.imdbRating}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Language: <span>{movieInfo?.Language}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Rated: <span>{movieInfo?.Rated}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Release: <span>{movieInfo?.Release}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Runtime: <span>{movieInfo?.Runtime}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Genre: <span>{movieInfo?.Genre}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Director: <span>{movieInfo?.Director}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Actors: <span>{movieInfo?.Actors}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Plot: <span>{movieInfo?.Plot}</span>
+            </MovieInfo>
+          </InfoColumn>
+          <Close onClick={() => props.onMovieSelect()}>X</Close>
+        </>
+      ) : (
+        "Loading..."
+      )}
     </Container>
-  )
+  );
 }
 
-export default MovieInfoComponent
+export default MovieInfoComponent;
